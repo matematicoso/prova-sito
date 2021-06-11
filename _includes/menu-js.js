0cd9bad4a964a2b3df08d4c5777497130f1903a1
Icaro <sognando.con.icaro@gmail.com>
2021-06-11 12:39:08 +0200

    Update menu-js.js

--- _includes/menu-js.js
+++ _includes/menu-js.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById("searchButton");
 function inputField(){
+  console.log(btn.classList)
   if (btn.classList.contains("clicked")){
     btn.classList.remove("clicked");
   }
